4918d2e79bfa267467d9c3f0cbd44e252eacbcf9
Alex Elmore <alexelmore1014@gmail.com>
2020-01-18 01:29:46 -0500

    added prompt

--- tracker.js
+++ tracker.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-
+const inquirer = require("inquirer");
 const connection = mysql.createConnection({
 host: "localhost",
 port: 3306,
@@ -7,3 +7,16 @@ user: "root",
 password: "sammyly456",
 database: "employees_db"
 });
+
+connection.connect(function(err) {
+if (err) throw err;
+console.log("Connected as id " + connection.threadId);
+startFunction();
+});
+
+function startFunction() {
+inquirer.prompt({
+name: "category",
+type: "list",
+message: "What will you be working on today?",
+choices: ["Departments", "Roles", "Employees","Exit"]}
